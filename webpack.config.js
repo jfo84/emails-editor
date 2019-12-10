7fe47cec9a381896b77797d5d308e98b0f4a1b04
Jack Ford <jford8820@gmail.com>
2019-12-10 22:30:04 +0100

    Add webpack-compression-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,44 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.tsx",
-  mode: "development",
+  plugins: [],
+};
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/,
         exclude: /(node_modules)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
       },
       {
         test: /\.css$/,
         exclude: /(node_modules)/,
-        use: ["style-loader", "css-loader"],
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
-  resolve: { extensions: [".ts", ".tsx", ".js", ".jsx", ".json"] },
+  resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
   },
-  devtool: "cheap-module-eval-source-map",
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    contentBase: path.join(__dirname, 'public/'),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [
+    new CompressionPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ]
 };
