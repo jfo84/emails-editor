b0d7efa35e7d73b1ef7dcf5299360e7aa757d549
jfo84 <jford8820@gmail.com>
2019-12-11 13:56:20 +0100

    Use 0.0.0.0 instead of localhost for deploy

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const http = require('http');
 const gzipStatic = require('connect-gzip-static');
 
 const app = connect();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const publicPath = '/dist';
 
 app.use(gzipStatic(__dirname + publicPath));
 
 const server = http.createServer(app);
 
-server.listen(
+server.listen({
   port,
-  () => console.log(`Listening to ${publicPath} on port ${port}`),
-);
+  host: '0.0.0.0',
+}, () => console.log(`Listening to ${publicPath} on port ${port}`));
